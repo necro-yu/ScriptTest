04dc990fa95663d0ede7aa59da1abee62e3e1f4e
ねくろまんさ <75344908+necro-yu@users.noreply.github.com>
2022-06-01 00:58:10 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;   // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; // 魔力
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    // 魔法攻撃用の関数
+    public void Magic()
+    {
+        if (this.mp >= 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+
+        // ---課題---
+        int[] array = { 1, 100, 39, 810, 777 };
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log("配列" + i + "の値：" + array[i]);
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log("配列" + i + "の値：" + array[i]);
+        }
+
+        // 発展課題
+        lastboss.Magic();
+        for (int i = 0; i < 10; i++)
+        {
+            lastboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
